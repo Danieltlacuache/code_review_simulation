bdcb843169e79280f99cd2eae21c50d8c9397f10
Leonel1803 <leonrivera2003@gmail.com>
2026-03-10 08:37:25 -0600

    Mejora de código

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 """
-Renombra la función y las variables siguiendo snake_case y nombres con sentido
+• Renombra la función y las variables siguiendo snake_case y nombres con sentido
 • Separa la lógica en al menos 3 funciones pequeñas y con un solo propósito
 • Maneja correctamente el error cuando el usuario escribe texto en vez de número (ValueError)
 • Agrega un mensaje de bienvenida + instrucciones claras
@@ -8,23 +8,46 @@ Renombra la función y las variables siguiendo snake_case y nombres con sentido
 
 import random
 
-def startGame():
-    number = random.randint(1, 20)
-    guess = 0
+def print_instructions():
+    print("¡Bienvenido al juego de adivinar el número!")
+    print("Instrucciones:")
+    print("1. El programa generará un número aleatorio entre 1 y 20.")
+    print("2. Tu objetivo es adivinar el número en el menor número de intentos posible.")
+    print("3. Después de cada intento, recibirás una pista si tu intento es muy bajo, muy alto o correcto.")
+    print("¡Buena suerte!")
+
+def print_result(guess_number, random_number):
+    if guess_number < random_number:
+        print("Muy bajo")
+    elif guess_number > random_number:
+        print("Muy alto")
+    elif guess_number == random_number:
+        print("¡Correcto!")
+    else:
+        print("Error")
+
+def get_user_guess():
+    while True:
+        try:
+            guess = int(input("Ingresa tu intento: "))
+            return guess
+        except ValueError:
+            print("Error: por favor ingresa un número válido.")
+
+
+def start_game():
+    random_number = random.randint(1, 20)
     attempts = 0
-    print("Adivina el número entre 1 y 20")
-    while guess != number:
-        guess = int(input("Ingresa tu intento: "))
+
+    print_instructions()
+    while True:
+        guess_number = get_user_guess()
         attempts += 1
-        if guess < number:
-            print("Muy bajo")
-        elif guess > number:
-            print("Muy alto")
-        elif guess == number:
-            print("¡Correcto!")
-        else:
-            print("Error")
+        print_result(guess_number, random_number)
+        if guess_number == random_number:
+            break
+
     print("Número de intentos:", attempts)
-startGame()
 
-
+if __name__ == "__main__":
+    start_game()
