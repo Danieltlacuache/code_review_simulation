6178cbce532de2f1703108f0bb94d4c457d1d33a
Emilio Daniel Guzmán Seda <eguzmanseda@gmail.com>
2026-03-10 08:50:45 -0600

    my version

--- code.py
+++ code.py
@@ -12,31 +12,51 @@ import random
 
 
 def print_instructions():
+    print()
     print("==============================================")
-    print("¡Bienvneido al juego de adivinar el número!")
+    print("¡Bienvenido al juego de adivinar el número!\n")
+    print("==============================================\n\n")
+    print("Instrucciones:\n")
+    print("1. El usuario debe de adivinar el número al azar que está entre\n" 
+            "el uno y el 20")
+    print("2. A partir del número ingresado el programa le va a indicar si\n" 
+    "dicho número fue muy bajo, mul alto o si adivinó\n\n")
 
-def start_game():
-    number = random.randint(1, 20)
-    guess = 0
-    attempts = 0
-    print("Adivina el número entre 1 y 20")
-    while guess != number:
-        guess = int(input("Ingresa tu intento: "))
-        attempts += 1
-    if guess < number:
-        print("Muy bajo")
-    elif guess > number:
-        print("Muy alto")
-    elif guess == number:
-        print("¡Correcto!")
-    else:
-        print("Error")
-    print("Número de intentos:", attempts)
 
 def input_game():
-    pass
+    try:
+        guess = int(input("Ingresa tu intento: "))
+        if(guess > 20 or guess <= 0):
+            print("valor inválido; debe de estar entre 1 y 20")
+        return guess
+    except ValueError:
+        print("valor inválido; debe de ser un número y estar en rango 1 y 20")
+    
 
 
 def validations_game():
-    pass
+    number = random.randint(1, 20)
+    guess = input_game()
+    attempts = 0
+    while guess != number:
+        attempts += 1
+        if guess < number:
+            print("Muy bajo\n")
+            guess = input_game()
+        elif guess > number:
+            print("Muy alto\n")
+            guess = input_game()
+        elif guess == number:
+            print("¡Correcto!\n")
+            return
+        else:
+            print("Error\n")
+    print("Número de intentos:", attempts)
+
+def main():
+    print_instructions()
+    print("Adivina el número entre 1 y 20")
+    validations_game()
+
+main()
 
